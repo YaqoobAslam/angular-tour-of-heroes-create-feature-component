64127b654ff91cf8936a10faa17d069356285767
Yaqoob <happyyaqoob@gmail.com>
2021-12-22 20:14:33 -0800

    initial commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,14 +9,16 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
 
-  constructor() { }
+selectedHero? :Hero;
 
-  ngOnInit(): void {
-  }
+constructor() { }
+
+ngOnInit(): void {
+}
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 
 }
